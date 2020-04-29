c92dd51823f758ea9a089d9673a750849164aeb4
Yash Vijay <yashvijay_17@yahoo.co.in>
2020-04-28 21:58:04 -0700

    Update bigrams.py
    Adding UTF - 8 encoding for file read and store. Also, fixed bug with ambiguous file shapes

--- bigrams.py
+++ bigrams.py
@@ -15,18 +15,18 @@ def WriteDataToCsv(dataList):
         writer.writerows(dataList)
 
 if __name__ == "__main__":
-    inputfile = 'C:\\Users\\yashv\\Desktop\\Merged.txt'
+    inputfile = 'C:\\Users\\yashv\\Desktop\\test.txt'
     
     # Input the file  
     txt1 = [] 
-    with open(inputfile) as file: 
+    with open(inputfile, encoding="utf-8") as file: 
         txt1 = file.readlines() 
 
     #stopword removal
     nltk.download('stopwords')
     nltk.download('punkt')
     all_stopwords = set(stopwords.words('english'))
-    new_words = ['2014','2016','2009','2017','2012','2011','2007','2006','2005','2018','in','the','x99s','x93','x9d','xe2','x80',',','.','(',')','4k','3-d','&',';','amp','_','[',']','it','also','asu','``','e.g','i','this','new',':','the','we','one','three','two','?','—','dr.','in','$','p.m.','5-12','yes','\'s','arizona','state','university','use','\'','7,000','at','25,000','an','each','that','first','these','but','43,000','53,000','us','most','as','ph.d.','try','\t','b','cv','ph.d.','â','arizona.â','cv\n\nâ','/','univ','y.','w.','v.','z.','university.â','467/567','six','-','h','r','az','co.','ltd.','e','h.','c.-f.','u.s.','u.s.â','universityâ','uk','wi.','x.','x',"na","'",'g','s.']
+    new_words = ['2020','2022','2024','2023','2021','2014','2016','2009','2017','2012','2011','2007','2006','2005','2018','in','the','x99s','x93','x9d','xe2','x80',',','.','(',')','4k','3-d','&',';','amp','_','[',']','it','also','asu','``','e.g','i','this','new',':','the','we','one','three','two','?','—','dr.','in','$','p.m.','5-12','yes','\'s','arizona','state','university','use','\'','7,000','at','25,000','an','each','that','first','these','but','43,000','53,000','us','most','as','ph.d.','try','\t','b','cv','ph.d.','â','arizona.â','cv\n\nâ','/','univ','y.','w.','v.','z.','university.â','467/567','six','-','h','r','az','co.','ltd.','e','h.','c.-f.','u.s.','u.s.â','universityâ','uk','wi.','x.','x',"na","'",'g','s.']
     new_stopwords_list = all_stopwords.union(new_words)
     for i, line in enumerate(txt1): 
         txt1[i] = ' '.join([x for x in nltk.word_tokenize(line) if ( x not in new_stopwords_list )]) 
@@ -52,15 +52,12 @@ if __name__ == "__main__":
     X1 = vectorizer.fit_transform(txt1)  
     features = (vectorizer.get_feature_names()) 
     # Code to change datatype and merge the two outputs
-    X2 = X1.toarray()
+    X1 = X1.toarray()
+    X2 = np.sum(X1, axis = 0)
     X2 = X2.tolist()
-    X3 = []
-    for row in X2:
-        for row2 in row:
-            X3.append(row2)
-    X5 = [(X3[i], features[i]) for i in range(0, len(X3))]
-    X6 = sorted(X5,key=lambda l:l[0], reverse = True)
-    WriteDataToCsv(X6)
+    X3 = list(zip(features, X2))
+    X3 = sorted(X3,key=lambda l:l[1], reverse=True)
+    WriteDataToCsv(X3)
     
     
     # Applying TFIDF 
